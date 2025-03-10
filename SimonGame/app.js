f957c2d2a0cd21ce03834875359c8fd15786a219
Rahat Ahmed <91186934+rahat605@users.noreply.github.com>
2025-03-11 00:38:38 +0600

    high_score_fix

--- SimonGame/app.js
+++ SimonGame/app.js
@@ -3,17 +3,19 @@ let userseq = [];
 let btns = ["yellow", "red", "green", "blue"];
 let started = false;
 let level = 0;
+let highLevel = 0;
 
 let h2 = document.querySelector("h2");
+let h3 = document.querySelector("h3");
+
+h3.innerText = `Highest Score : ${highLevel}`;
 
 document.addEventListener("keypress", function () {
   if (started == false) {
-    console.log("Game is started!");
     started = true;
     levelUp();
   }
 });
-
 function btnFlash(btn) {
   btn.classList.add("flash");
   setTimeout(function () {
@@ -31,12 +33,15 @@ function usrFlash(btn) {
 function levelUp() {
   userseq = [];
   level++;
+  if(level > highLevel){
+    highLevel++;
+    h3.innerText = `Highest Score : ${highLevel}`;
+  }
   h2.innerText = `Level ${level}`;
   let randIdx = Math.floor(Math.random() * 3);
   let ranColor = btns[randIdx];
   let randbtn = document.querySelector(`.${ranColor}`);
   gameSeq.push(ranColor);
-  console.log(gameSeq);
   btnFlash(randbtn);
 }
 
@@ -46,7 +51,12 @@ function checkAns(idx) {
       setTimeout(levelUp(), 1000);
     }
   } else {
-    console.log("wrong start over");
+    h2.innerText = `Game Over\n Your Score: ${level} \n Press any key to play again`;
+    document.querySelector("body").style.backgroundColor = "red";
+    setTimeout(function () {
+      document.querySelector("body").style.backgroundColor = "white";
+    }, 100);
+    reset();
   }
 }
 
@@ -63,4 +73,11 @@ let allBtns = document.querySelectorAll(".btn");
 
 for (btn of allBtns) {
   btn.addEventListener("click", btnPress);
+}
+
+function reset() {
+  started = false;
+  gameSeq = [];
+  userseq = [];
+  level = 0;
 }
